f30fa2fe34822ae3cd729bc6bad848a490b7aa7c
Jeffrey Allen <jeffwayne256@gmail.com>
2022-10-19 16:46:24 -0500

    Added nginx proxy that serves static images and deploy file

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,31 @@ FROM python:3.10.2-slim-bullseye
 ENV PIP_DISABLE_PIP_VERSION_CHECK 1
 ENV PYTHONDONOTWRITEBYTECODE 1
 ENV PYTHONNUMBEFFERED 1
+ENV PATH="/scripts:${PATH}"
 
 # Set work directory
 WORKDIR /code
 
 # Install dependencies
 COPY ./requirements.txt .
+RUN echo 'deb http://deb.debian.org/debian testing main' >> /etc/apt/sources.list
+RUN apt-get update -y
+RUN apt-get install -y gcc
+RUN rm -rf /var/lib/apt/lists/*
 RUN pip install -r requirements.txt
 
 # Copy project
+COPY ./scripts /scripts
 COPY . .
+
+RUN chmod +x /scripts/*
+
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
+
+RUN adduser --disabled-login nginxuser
+RUN chown -R nginxuser:nginxuser /vol
+RUN chmod -R 755 /vol/web
+USER nginxuser
+
+CMD ["entrypoint.sh"]
--- eileensrecipebook/settings.py
+++ eileensrecipebook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k*4*jogptkx7r2yre1brfr%%c$c^9aoh)t#93&j0n$8e2_)v^u'
+#SECRET_KEY = 'django-insecure-k*4*jogptkx7r2yre1brfr%%c$c^9aoh)t#93&j0n$8e2_)v^u'
+SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 
 # Application definition
@@ -119,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL  = '/static/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = 'vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
